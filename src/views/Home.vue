4814f854caa6f0596a84ecc5faeadfce9f901577
Cherylee <1065049122@qq.com>
2020-09-28 08:45:03 +0800

    p

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    1111888899988889999
+    111188
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
